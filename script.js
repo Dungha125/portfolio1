55200b47b0686510f4103e594ef7c20fef0d66e3
Dungha125 <dungha122405@gmail.com>
2023-05-19 00:48:19 +0700

    dungha

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
-import axios from 'axios';
 
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+}
+
+window.addEventListener('scroll', function() {
+    var backToTopButton = document.getElementById('backtotop');
+    if (window.scrollY > 0) {
+        backToTopButton.style.display = 'block';
+    } else {
+        backToTopButton.style.display = 'none';
+    }
+});
+
+document.getElementById('backtotop').addEventListener('click', scrollToTop);
